e7fcd1e61bdbbe3c5a7ca38a73305068167233b4
henrique-naldy <Henrique.nordy@gmail.com>
2024-11-28 17:41:07 -0300

    update

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-
+from models.task import Task
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world!"
+# CRUD
+#Create, Read, Update and Delete = Criar, ler, Atualizar e Deletar
+#Tabela: Tarefa
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+tasks = []
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
