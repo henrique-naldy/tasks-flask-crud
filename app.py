2b5f67c294c27d1f59a2f4abfa5c499f1bec69f4
Henrique Naldy <Henrique.nordy@gmail.com>
2024-12-03 23:14:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,8 @@ def update_task(id):
     for t in tasks:
         if t.id == id:
             task =t
-    print(task)
+            break
+        
     if task == None:
         return jsonify({"message": "não foi possível encotnrar a atividade"}), 404
 
@@ -51,6 +52,21 @@ def update_task(id):
     task.completed = data['completed']
     print(task)
     return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break    
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
